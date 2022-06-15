445951e432b4a89986a40971d5cc60466ef40ce8
Rayyouu <rayane.ainseba@ynov.com>
2022-06-15 06:05:05 +0200

    fix controller

--- models/player.py
+++ models/player.py
@@ -6,10 +6,27 @@ import pygame
 from models.projectile import Projectile
 
 
-
-
 class Player(pygame.sprite.Sprite):
     def __init__(self, id, v_rotation, velocity, health, power_shoot, cooldown, speed_shoot, game):
+        self.boutons_ps4_controllers = {
+            "x": 0,
+            "circle": 1,
+            "square": 2,
+            "triangle": 3,
+            "share": 4,
+            "PS": 5,
+            "options": 6,
+            "left_stick_click": 7,
+            "right_stick_click": 8,
+            "L1": 9,
+            "R1": 10,
+            "up_arrow": 11,
+            "down_arrow": 12,
+            "left_arrow": 13,
+            "right_arrow": 14,
+            "touchpad": 15
+        }
+        
         super().__init__()
         self.game = game
         # id pour différencier le joueur 1 et 2 (et 3, 4 si besoin)
@@ -46,8 +63,7 @@ class Player(pygame.sprite.Sprite):
         if self.health <= 0:
             return True
         return False
-        
-    
+
     def health_bar(self, surface):
         color_health = (111, 210, 46)
         back_color_health = (60, 60, 60)
@@ -62,10 +78,9 @@ class Player(pygame.sprite.Sprite):
 
     def launch_projectile(self):
         """Ajoute un projectile"""
-        if pygame.time.get_ticks() - self.last_time_when_projectile_launch >= self.cooldown * 1000 :
+        if pygame.time.get_ticks() - self.last_time_when_projectile_launch >= self.cooldown * 1000:
             self.all_projectiles.add(Projectile(self))
             self.last_time_when_projectile_launch = pygame.time.get_ticks()
-        
 
     def update_pos(self, screen):
         """Fonction qui va mettre à jour la position du joueur (en fonction des touches enfoncée)"""
@@ -74,27 +89,24 @@ class Player(pygame.sprite.Sprite):
         if self.pressed.get(pygame.K_RIGHT) and self.rect.x + self.rect.width < screen.get_width():
             self.move_right()
 
-       
         if self.pressed.get(pygame.K_LEFT) and self.rect.x > 0:
             self.move_left()
 
-       
         if self.pressed.get(pygame.K_UP) and self.rect.y > 0:
             self.move_up()
 
-        
         if self.pressed.get(pygame.K_DOWN) and self.rect.y + self.rect.height < screen.get_height():
             self.move_down()
 
-        if self.pressed.get(pygame.K_SPACE):
+        if self.pressed.get(pygame.K_SPACE) or self.pressed.get(self.boutons_ps4_controllers["x"]):
             self.launch_projectile()
 
         # Touche pour checker la rotation
-        if self.pressed.get(pygame.K_x):
+        if self.pressed.get(pygame.K_x) or self.pressed.get(self.boutons_ps4_controllers["L1"]):
             self.rot_center(self.angle + self.v_rotation)
 
         # Pareil mais de l'autre côté
-        if self.pressed.get(pygame.K_c):
+        if self.pressed.get(pygame.K_c  or self.pressed.get(self.boutons_ps4_controllers["R1"])):
             self.rot_center(self.angle - self.v_rotation)
 
     def rot_center(self, angle):
@@ -105,23 +117,23 @@ class Player(pygame.sprite.Sprite):
     def move_right(self):
         print("Déplacement vers la droite")
         self.rect.x += self.velocity
-        if Lib.check_colliders(self, self.game.player2_group):
+        if Lib.check_colliders(self, self.game.player2_group if self.id == 1 else self.game.player1_group):
             self.rect.x -= self.velocity
 
     def move_left(self):
         print("Déplacement vers la gauche")
         self.rect.x -= self.velocity
-        if Lib.check_colliders(self, self.game.player2_group):
+        if Lib.check_colliders(self, self.game.player2_group if self.id == 1 else self.game.player1_group):
             self.rect.x += self.velocity
 
     def move_up(self):
         print("Déplacement vers le haut")
         self.rect.y -= self.velocity
-        if Lib.check_colliders(self, self.game.player2_group):
+        if Lib.check_colliders(self, self.game.player2_group if self.id == 1 else self.game.player1_group):
             self.rect.y += self.velocity
 
     def move_down(self):
         print("Déplacement vers le bas")
         self.rect.y += self.velocity
-        if Lib.check_colliders(self, self.game.player2_group):
+        if Lib.check_colliders(self, self.game.player2_group if self.id == 1 else self.game.player1_group):
             self.rect.y -= self.velocity
